9b18e91e7345aa447ff54ec874e74d6e3fe2b029
NILESH211807 <nileshkumar0815@gmail.com>
2025-04-29 12:56:10 +0530

    register and login route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Loader from './components/Loader';
+const Home = lazy(() => import("./pages/Home"));
+const Register = lazy(() => import("./pages/Register"));
+const Login = lazy(() => import("./pages/Login"));
+
 const App = () => {
-  return (
-    <div className='bg-red-500 text-white'>
-      App
-    </div>
-  )
+    return (
+        <Suspense fallback={<div className='w-full h-screen flex items-center justify-center'><Loader /></div>}>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </Suspense>
+    )
 }
 
 export default App
