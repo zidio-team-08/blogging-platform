1ee8c4da21fa9d200c37d651bf2cda0262adbe70
NILESH211807 <nileshkumar0815@gmail.com>
2025-04-30 12:21:13 +0530

    backend auth fixed some issues

--- api/app.js
+++ api/app.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,18 +11,18 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //database connection
 connectDB();
 
+//Routes
+app.use("/api/auth", authRoute);
+
 app.get('/', (req, res) => {
     res.send("Hello..!");
 });
-
-
-//Routes
-app.use("/", userRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
