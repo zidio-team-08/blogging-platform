3ee5545bafe0a70efc66d7a028c73821793a0a9e
NILESH211807 <nileshkumar0815@gmail.com>
2025-05-26 09:27:16 +0530

    few more frontend changes

--- client/src/hook/useAxios.js
+++ client/src/hook/useAxios.js
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../utils/axiosInstance';
-import { handleResponse } from '../utils/responseHandler';
 
 const useAxios = () => {
 
--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -3,5 +3,4 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URI,
     withCredentials: true,
-    timeout: 10000,
 });
